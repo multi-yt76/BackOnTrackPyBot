2abb95eee0eb55c26e7825b20d5cdd0272055b4e
Multi Productions <aruna.kaleeswaran@gmail.com>
2021-03-07 13:11:49 +0000

    Changes for Episode 2

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# Episode 1 ->
+
 import discord
 import os
 from discord.ext import commands
+from typing import Union
 
 bot = commands.Bot(command_prefix = "$")
 
@@ -15,5 +18,17 @@ async def ping(ctx):
 @bot.command()
 async def add(ctx, arg1 : int, arg2 : int):
   await ctx.send(f"The answer is {arg1 + arg2}")
+
+# .. <-
+
+# Episode 2 ->
+
+@bot.command()
+async def msg(ctx, arg : Union[discord.TextChannel, discord.Member], *, msg):
+  if type(arg) == discord.Member:
+    await arg.send(f"Sending Message To User: {msg}")
+
+  elif type(arg) == discord.TextChannel:
+    await arg.send(f"Sending Message To Channel: {msg}")
 
 bot.run(os.environ.get("TOKEN"))
